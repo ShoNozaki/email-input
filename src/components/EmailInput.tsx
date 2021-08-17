33e73f83fcc15b1276bc31c6003993e447c57096
Sho <sho.nozaki417@gmail.com>
2021-08-17 13:59:22 -0700

    added hardcoded tag to pass test

--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import {data} from '../data'
+
+// console.log(data)
 
 const EmailInput = () => {
 	return (
-		<div>
+		<div className="emailInput">
+			<div className="tag">
+				<span>theresa@outlook.com</span>
+				<button className="deleteButton">x</button>
+			</div>
 			<input placeholder="Enter recipients..."></input>
 		</div>
 	)
