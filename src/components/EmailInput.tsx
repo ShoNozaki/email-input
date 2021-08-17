5d3a04453df24437c8cf8db97ffa6a331b44f28c
Sho <sho.nozaki417@gmail.com>
2021-08-17 15:31:30 -0700

    added function to delete tag

--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {data} from '../data'
+import './EmailInput.css'
 
 // console.log(data)
 
+
 const EmailInput = () => {
+	const [tags, setTags] = useState(["theresa@outlook.com", "erictaylor"]);
+
+	const removeTag = (tagI: number) => {
+		setTags(prevTags => prevTags.filter((tag, i) => i !== tagI))
+	}
+
+
 	return (
 		<div className="emailInput">
-			<div className="tag">
-				<span>theresa@outlook.com</span>
-				<button className="deleteButton">x</button>
-			</div>
+			{tags.map((tag, tagI) => {
+				return (
+				<div className="tag">
+					<span>{tag}</span>
+					<button 
+					className="deleteButton"
+					onClick={() => removeTag(tagI)}
+					>x</button>
+				</div>
+				)
+			})}
 			<input placeholder="Enter recipients..."></input>
 		</div>
 	)
