890d6bcfa90c0294f08d1e0f131baa0e2293838b
Sho <sho.nozaki417@gmail.com>
2021-08-17 13:51:24 -0700

    added test for rendering test tag

--- src/components/EmailInput.test.tsx
+++ src/components/EmailInput.test.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { screen, render } from '@testing-library/react'
 import EmailInput from './EmailInput'
 
-test('Renders input', () => {
+test('Renders input and tags', () => {
 	render(<EmailInput />)
 	expect(screen.getByPlaceholderText('Enter recipients...')).toBeInTheDocument()
+	expect(screen.getByText('theresa@outlook.com')).toBeInTheDocument()
+	expect(screen.getByRole('button')).toHaveTextContent('x')
 })
